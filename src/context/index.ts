3b751afda0fa1279a0f6519416821c3fcb3be5b7
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2021-01-11 21:06:22 +0100

    change export path of ctx type reviver

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,13 +1,4 @@
-import { Request, Response} from 'express';
-
 export interface RequestContext {
   request: any;
   response: any;
-}
-
-export function accessRequestContext(context: RequestContext) {
-  return {
-    request: context.request as Request,
-    response: context.response as Response,
-  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 export { router } from './router';
 export { server } from './server';
 export * from './types';
+
+import { Request, Response} from 'express';
+import { RequestContext } from './context';
+
+export function accessRequestContext(context: RequestContext) {
+  return {
+    request: context.request as Request,
+    response: context.response as Response,
+  }
+}
