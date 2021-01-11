11e40a39eccbd7ffadcf6f0371ba8bacd625e0e7
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2021-01-11 21:03:21 +0100

    add a concealed type reviver

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,4 +1,13 @@
+import { Request, Response} from 'express';
+
 export interface RequestContext {
   request: any;
   response: any;
+}
+
+export function accessRequestContext(context: RequestContext) {
+  return {
+    request: context.request as Request,
+    response: context.response as Response,
+  }
 }
